467fa83f3dd69ce5b26cb6e3e5c90c7c4a250000
ReeseJones <reesedrjones@gmail.com>
2024-01-27 11:16:35 -0800

    no longe sync files in public

--- public/bundle.js
+++
@@ -1,2 +0,0 @@
-"use strict";function t(){return`(${this.x}, ${this.y})`}const e={x:(n={x:0,y:3}).x+(o={x:-3,y:32}).x,y:n.y+o.y,toString:t};var n,o;console.log(`Webpage loaded! ${Date.now()} and this added is:${e}`);
-//# sourceMappingURL=bundle.js.map
